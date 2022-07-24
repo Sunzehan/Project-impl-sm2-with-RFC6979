fb2c7444153821b7a0643b900a92f4077b31657e
Sunzehan <107350922+Sunzehan@users.noreply.github.com>
2022-07-24 20:27:17 +0800

    Update Main_Part.py

--- Main_Part.py
+++ Main_Part.py
@@ -1,7 +1,9 @@
+#测试主体程序，调用key_Enc_test.py里的代码进行测试
+
 if __name__ == "__main__":
     test_ECDH()
     test_SM2_agreement(True)
-    # 可复现SM2文档中的示例结果
+    # 运行后可复现SM2文档中的示例结果
     test_signature()
     test_SM2_agreement2(True)
     test_encryption()
